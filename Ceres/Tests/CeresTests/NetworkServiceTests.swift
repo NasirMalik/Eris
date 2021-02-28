8c9892e514ef3c4a8a9f289e15699e14c5761c73
NaxirMahmood <nasir.mahmood@deliveryhero.com>
2021-02-28 21:37:18 +0100

    tests name correction

--- Ceres/Tests/CeresTests/NetworkServiceTests.swift
+++ Ceres/Tests/CeresTests/NetworkServiceTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import Ceres
 
-final class CeresServiceTests: XCTestCase {
+final class NetworkServiceTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
